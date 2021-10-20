860e482ee5b07e1f10a6a69506768c8dd9b4b455
Erik Dan Tran <erikdantran99@gmail.com>
2021-10-20 00:21:39 -0500

    Completed up to level 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,90 @@
+import axios from 'axios';
 import React from 'react';
+import MovieList from './movieList.jsx';
+import Search from './search.jsx';
+import AddMovie from './addMovie.jsx';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: null,
+
+      searching: false,
+      searchResults: null,
+      noMovies: false,
+
+      watched: [],
+      displayWatched: false
+    }
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleFinishSearch = this.handleFinishSearch.bind(this)
+    this.getData = this.getData.bind(this); // may not need to bind this
+    this.postData = this.postData.bind(this);
+    this.addToWatched = this.addToWatched.bind(this);
+    }
+
+  componentDidMount = () => {
+    this.getData();
+  }
+
+  getData = () => {
+    axios.get('http://localhost:3000/movies')
+      .then((results) => {
+        this.setState({ movies: results.data });
+      })
+  }
+
+  postData = (data) => {
+    axios.post('http://localhost:3000/movies', data)
+      .then((results) => {
+        this.getData()
+      })
+  }
+
+  handleSearch = (q) => {
+    var queryResults = this.state.movies.filter(movie => movie.title.includes(q));
+    if (queryResults.length === 0) { this.setState({ noMovies: true }) }
+    this.setState({
+      searching: true,
+      searchResults: queryResults
+    })
+  }
+
+  handleFinishSearch = () => {
+    this.setState({
+      searching: false,
+      searchResults: null,
+      noMovies: false
+    })
+  }
+
+  addToWatched = (data) => {
+    this.setState({watched: [...this.state.watched, data]})
+  };
+
+  removefromWatched = (data) => {
+    this.setState({watched: this.state.watched.filter((movie) => {
+      return movie !== data;
+    })})
+  }
+
+  render() {
+
+    return (
+      <div className="main">
+        <h2>Movie List</h2>
+        <AddMovie add={this.postData} />
+        <Search search={this.handleSearch} searchOver={this.handleFinishSearch} />
+        {this.state.noMovies ? <span>No movies found</span> : null}
+
+        {this.state.movies === null ? null :
+          this.state.searching ? <MovieList movies={this.state.searchResults} /> : <MovieList movies={this.state.movies} />}
+
+      </div>
+    )
+  }
+}
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
-const express = require('express');
-const app = express();
-const PORT = 3000 || process.env.PORT;
+var express = require('express')
+var app = express();
+var helpers = require('./dbHelpers.js')
+var port = 3000;
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')))
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', {root: path.join(__dirname, '../client/dist')})
+// })
+
+app.get('/movies', (req, res) => {
+  helpers.get()
+    .then((results) => {
+      res.json(results);
+    })
+});
+
+app.post('/movies', (req, res) => {
+  helpers.post(req.body)
+    .then((results) => {
+      res.send('Insert successsful')
+    })
+});
+
+app.listen(port, () => {
+  console.log(`listening on https/localhost:${port}`)
 })
